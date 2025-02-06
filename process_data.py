c4b63ae58c2b6d7ccd621f66cb5ce757bb253645
will5156 <wmgoble01@bvsd.org>
2025-02-06 22:18:45 +0000

    cool

--- process_data.py
+++ process_data.py
@@ -9,6 +9,8 @@ name - date
 import csv
 from typing import Union
 
+record = dict[str, Union[str, int, float]]
+
 def get_records(data_filename: str) -> list[dict[str, Union[str, int]]]:
   """
   Gets records from a csv data file.
@@ -26,17 +28,45 @@ def get_records(data_filename: str) -> list[dict[str, Union[str, int]]]:
       records.append(record)
 
   return records
-def convert_fields(records: list[dict[str, Union[str, int]]]) -> None:
+
+def convert_fields(records: list[record]) -> None:
   for record in records:
-    record["REPORTYEAR"] = int[record["REPORTYEAR"]]
+    record["REPORTYEAR"] = int(record["REPORTYEAR"])
+    record["PropValue"] = float(record["PropValue"])
+
+def sumarize_by_month(records: list[record]) -> dict[str, int]:
+  summary: dict[str, int] = {}
+
+  for r in records:
+    month: str = r["REPORTDATE"][5:7] #grab month
+    if month in summary:
+      summary[month] += 1
+    else:
+      summary[month] = 1
+  
+  return summary
+def sumarize_by_year(records: list[record]) -> dict[str, int]:
+  summary: dict[str, int] = {}
+
+  for r in records:
+    month: str = r["REPORTYEAR"][5:7] #grab month
+    if month in summary:
+      summary[month] += 1
+    else:
+      summary[month] = 1
+  
+  return summary
+
 
 def main() -> None:
   data_filename: str = "resources/stolen_bikes.csv"
   records: list[dict[str, Union[str, int]]] = get_records(data_filename)
+  convert_fields(records)
 
+  summary: dict[str, int] = sumarize_by_month(records)
   print(f"{len(records)} records read in.")
-  print(records[0])
-  print(records[0]["District"])
+  for month in summary.keys():
+    print(f"{month}: {summary[month]}")
 
 
 if __name__ == "__main__":
